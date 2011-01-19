a405cc50d6c19c890762b042522a6ab1b64da08e
sharman <neeraj.s.sharma@gmail.com>
2011-01-19 11:11:03 -0600

    added a system.out message to the main class

--- src/MinneTravelMain.java
+++ src/MinneTravelMain.java
@@ -4,6 +4,7 @@ import net.rim.device.api.ui.UiApplication;
 public class MinneTravelMain extends UiApplication{
 
 	public static void main(String[] args) {
+		System.out.println(" Main class of MinneTravel BlackBerry Application");
 	}
 
 }
